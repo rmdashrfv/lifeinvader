a560fa2e072381eda83e4303613543ac6494267f
Michael Law <michaellaw@gmx.com>
2022-04-07 17:33:26 -0400

    Added basic component structure

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import Timeline from "./Timeline"
+import Sidebar from "./Sidebar"
+
+const Homepage = () => {
+  return(
+    <div>
+      <h2>Welcome to Lifeinvader</h2>
+      <Timeline />
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Homepage;
