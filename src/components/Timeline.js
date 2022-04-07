ca3393316ac8f47d3e701c6e47e9989a1e378f6b
Michael Law <michaellaw@gmx.com>
2022-04-07 18:01:45 -0400

    Added posts request

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from 'react';
+
 const Timeline = () => {
+  const [posts, setPosts] = useState([])
+  
+  useEffect(async () => {
+    console.log('Rendering Timeline ...')
+    let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
+    let res = await req.json()
+    setPosts(res)
+  }, [])
+  
   return (
     <div>
       <h2>Timeline</h2>
