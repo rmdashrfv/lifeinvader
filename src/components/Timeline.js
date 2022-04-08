ba0a66a729334c9187ecafe1c6a8f737e8559757
Michael Law <michaellaw@gmx.com>
2022-04-07 20:23:37 -0400

    replaced data

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,16 +3,27 @@ import { useEffect, useState } from 'react';
 const Timeline = () => {
   const [posts, setPosts] = useState([])
   
-  useEffect(async () => {
-    console.log('Rendering Timeline ...')
-    let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
-    let res = await req.json()
-    setPosts(res)
+  useEffect(() => {
+    (async () => {
+      console.log('Rendering Timeline ...', new Date())
+      let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
+      let res = await req.json()
+      setPosts(res)
+    })()
   }, [])
   
   return (
     <div>
       <h2>Timeline</h2>
+      {
+        posts.map((element) => {
+          return(
+            <div key={element.id}>
+              {element.id}
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
