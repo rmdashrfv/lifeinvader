636c492e88dad71887f75eb587f7134ce094dc65
Michael Law <michaellaw@gmx.com>
2022-04-08 14:34:06 -0400

    Added navbar component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,11 @@
 import Timeline from "./Timeline"
 import Sidebar from "./Sidebar"
+import Navbar from "./Navbar"
 
 const Homepage = () => {
   return(
     <div>
+      <Navbar />
       <Timeline />
       <Sidebar />
     </div>
