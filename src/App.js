5b5a5c8e0d689049704341374f28505daea2bacb
Michael Law <michaellaw@gmx.com>
2022-04-08 13:25:16 -0400

    Added Posts to Timeline

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Homepage from './components/Homepage'
 
 function App() {
+  
   return (
-    <div>
+    <>
       <Homepage />
-    </div>
+    </>
   );
 }
 
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,10 +2,10 @@ import { useEffect, useState } from 'react';
 
 const Timeline = () => {
   const [posts, setPosts] = useState([])
-  
+
   useEffect(() => {
+    // IIFE: immediately invoked function expression
     (async () => {
-      console.log('Rendering Timeline ...', new Date())
       let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/lifeinvader/main/src/data/posts.json')
       let res = await req.json()
       setPosts(res)
@@ -18,8 +18,9 @@ const Timeline = () => {
       {
         posts.map((element) => {
           return(
-            <div key={element.id}>
-              {element.id}
+            <div>
+              <p>Post by {element.username}</p>
+              <p>{element.content}</p>
             </div>
           )
         })
