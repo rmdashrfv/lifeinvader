1356cc6279e12f573ad7cf45f29c39132d817b48
Michael Law <michaellaw@gmx.com>
2022-04-08 14:27:48 -0400

    Added FontAwesome

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,7 +7,9 @@ const Post = ({post}) => {
     <div>
       <img src={post.image} height="250" width="250" />
       <p>Post by {post.username} • {likes} likes</p>
-      <button onClick={() => { setLikes(likes + 1)}}>❤️</button>
+      <button onClick={() => { setLikes(likes + 1)}}>
+        <i class="fa-solid fa-heart" style={{color: '#d33'}}></i>
+      </button>
       <p>{post.content}</p>
     </div>
   )
